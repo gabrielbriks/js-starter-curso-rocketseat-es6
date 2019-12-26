f3106030a5668aff3dd5ba0fa845e714ae6207ac
gabrielbriks <greis9010@gmail.com>
2019-12-26 18:57:43 -0300

    Classes - Initial - Utilização de classes exemplo ToDos

--- bundle.js
+++ bundle.js
@@ -1,26 +1,83 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-// alert('teste');
-var teste =
+//criando uma classe 
+var List =
 /*#__PURE__*/
 function () {
-  function teste() {
-    _classCallCheck(this, teste);
-  }
+  //instanciando um contrutor
+  function List() {
+    _classCallCheck(this, List);
 
-  _createClass(teste, [{
-    key: "metodo",
-    value: function metodo() {}
-  }, {
-    key: "outro",
-    value: function outro() {}
+    this.data = [];
+  } //criando um metodo para adicionar uma informação e realizar o print no console
+
+
+  _createClass(List, [{
+    key: "add",
+    value: function add(data) {
+      this.data.push(data);
+      console.log(this.data);
+    }
   }]);
 
-  return teste;
-}();
+  return List;
+}(); //criando outra classe que herda a a classe list
+
+
+var TodoList =
+/*#__PURE__*/
+function (_List) {
+  _inherits(TodoList, _List);
+
+  //definimos novamente o contrutor
+  function TodoList() {
+    var _this;
+
+    _classCallCheck(this, TodoList);
+
+    //desse modo chamamos o construtor da nossa classe pai 'List'
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(TodoList).call(this)); //adicionando mais uma informação para a classe
+
+    _this.usuario = 'juca';
+    return _this;
+  } //criando metodo para printar no console o usuario
+
+
+  _createClass(TodoList, [{
+    key: "mostraUsuario",
+    value: function mostraUsuario() {
+      console.log(this.usuario);
+    }
+  }]);
+
+  return TodoList;
+}(List); //instanciando a classe TodoList
+
+
+var MinhaLista = new TodoList(); //Pegando o elemento html e atribuindo o envento click
+
+document.getElementById('novotodo').onclick = function () {
+  //chamando o nosso metodo de dentro da classe para adicionar um novo todo
+  MinhaLista.add('Novo Todo');
+}; //chamando o nosso outro metodo dentro da classe para mostrar o usuario
+
+
+MinhaLista.mostraUsuario();
--- main.js
+++ main.js
@@ -1,10 +1,39 @@
-// alert('teste');
-
-class teste{
-  metodo(){
-
+//criando uma classe 
+class List{
+  //instanciando um contrutor
+  constructor(){
+    this.data = [];
   }
-  outro(){
-
+  //criando um metodo para adicionar uma informação e realizar o print no console
+  add(data){
+    this.data.push(data);
+    console.log(this.data);
   }
 }
+
+//criando outra classe que herda a a classe list
+class TodoList extends List{
+  //definimos novamente o contrutor
+ constructor(){
+   //desse modo chamamos o construtor da nossa classe pai 'List'
+   super();
+    //adicionando mais uma informação para a classe
+   this.usuario = 'juca';
+ }
+ //criando metodo para printar no console o usuario
+ mostraUsuario(){
+   console.log(this.usuario);
+ }
+}
+
+//instanciando a classe TodoList
+const MinhaLista = new TodoList();
+
+//Pegando o elemento html e atribuindo o envento click
+document.getElementById('novotodo').onclick = function(){
+  //chamando o nosso metodo de dentro da classe para adicionar um novo todo
+  MinhaLista.add('Novo Todo');
+
+}
+//chamando o nosso outro metodo dentro da classe para mostrar o usuario
+MinhaLista.mostraUsuario();
