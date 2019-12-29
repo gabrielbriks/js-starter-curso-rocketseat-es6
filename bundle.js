27b3cd7376c3e4ed341916930eeb9a725e1c5f3e
gabrielbriks <greis9010@gmail.com>
2019-12-29 10:26:37 -0300

    Valores Padroes - entendendo como definir valores padroes; utilizando arrow functions

--- bundle.js
+++ bundle.js
@@ -1,75 +1,39 @@
 "use strict";
 
-var arr = [1, 2, 3, 4, 5]; // ---- ARROW FUNCTIONS----
-
-/**quando temos uma função anonima, eé o melhor local para utilizarmos
-* nossas arrow functions; da seguinte formas:
-*/
-//retirando o nome function
-
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr); //por termo apenas um parametro, podemos retirar os parenteses
-
-var newArr2 = arr.map(function (item) {
-  return item * 3;
-});
-console.log(newArr2);
-/**
- * quando nossa function retorna uma simples informação,
- * aí entao podemos reduzir ainda mais a nossa function; 
-*/
-
-var newArr3 = arr.map(function (item) {
-  return item * 4;
-});
-console.log(newArr3); //é possivel tambem utilizar-mos a 'const' para declarar functions
-//da seguinte maneira: ; apesar de nao ser recomendado...
-
-var teste = function teste() {
-  return 'teste';
-}; //EM VEZ DE
-// @ Assim fica mais bonito 
-// function teste(){
-// }
-
-
-console.log(teste()); //CASO QUISESSIMOS RETURNAR UM ARRAY, COM ESSA NOMENCLAURA:
-
-var teste2 = function teste2() {
-  return [1, 2, 5];
-};
-
-console.log(teste2()); //E SE TENTARMOS RETORNAR UM OBJETO DA SEGUINTE MANEIRA?
-
-var testeOBJ = function testeOBJ() {
-  nome: 'gabriel';
-};
-
-console.log(testeOBJ()); //resultado : undefined
+// VALORES PADRÕES  
 
 /**
- * ISSSO NAO FUNCIONOU POIS QUANDO UTILIZAMOS AS CHAVES, QUE TAMBEM FAZ
- * ELE ACABA FAZENDO PARTE DO PROPRIO CORPO DA FUNCTION, ENTAO NAO PODEMOS
- * UTILIZAR CHAVES PARA O OBJETO 
- * 
- * PARA ISSO TEMOS OUTRAS OPÇOES:
+ * Se por acaso tentarmos chamar essa functio apenas com um parametro,
+ * ou sem nenhum ????
 */
-//opcao 1
+function soma(a, b) {
+  return a + b;
+}
 
-var retObj = function retObj() {
-  return {
-    nome: 'gabriel'
-  };
+console.log(soma(1));
+console.log(soma());
+/**
+ * resultado: NaN
+ * esse resultado significa que é um resultado descnhecido,
+ * por exemplo que ele nao conseguiu realizar a soma, entao
+ * por consequencia ele retorna essa expressao 'NaN'
+*/
+//Para isso não acontecer denifimos os valores padroes;
+
+function somaSemError() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 2;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3;
+  return a + b;
+}
+
+console.log(somaSemError(1));
+console.log(somaSemError()); //UTILIZANDO A ARROW FUNCTIONS
+
+var somaArrowFunc = function somaArrowFunc() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return a + b;
 };
 
-console.log(retObj()); //opcao 2
-
-var retObj2 = function retObj2() {
-  return {
-    nome: 'gabriel2 '
-  };
-};
-
-console.log(retObj2());
+console.log(somaArrowFunc(1));
+console.log(somaArrowFunc());
--- main.js
+++ main.js
@@ -1,68 +1,33 @@
-const arr = [1,2,3,4,5];
-// ---- ARROW FUNCTIONS----
-/**quando temos uma função anonima, eé o melhor local para utilizarmos
-* nossas arrow functions; da seguinte formas:
-*/
 
-//retirando o nome function
-const newArr = arr.map((item)=>{
-  return item * 2;
-});
-
-console.log(newArr);
-
-//por termo apenas um parametro, podemos retirar os parenteses
-const newArr2 = arr.map(item=>{
-  return item * 3;
-});
-
-console.log(newArr2);
+// VALORES PADRÕES  
 
 /**
- * quando nossa function retorna uma simples informação,
- * aí entao podemos reduzir ainda mais a nossa function; 
+ * Se por acaso tentarmos chamar essa functio apenas com um parametro,
+ * ou sem nenhum ????
 */
-const newArr3 = arr.map(item => item * 4);
-
-console.log(newArr3);
-
-//é possivel tambem utilizar-mos a 'const' para declarar functions
-//da seguinte maneira: ; apesar de nao ser recomendado...
-const teste = () => {
-  return 'teste';
+function soma(a,b){
+  return a + b;
 }
-//EM VEZ DE
-// @ Assim fica mais bonito 
-// function teste(){
-
-// }
-console.log(teste());
-
-//CASO QUISESSIMOS RETURNAR UM ARRAY, COM ESSA NOMENCLAURA:
-
-const teste2 = () => [1,2,5];
-console.log(teste2())
-
-//E SE TENTARMOS RETORNAR UM OBJETO DA SEGUINTE MANEIRA?
-const testeOBJ = () => {nome:'gabriel'};
-
-console.log(testeOBJ());
-//resultado : undefined
-
+console.log(soma(1));
+console.log(soma());
 /**
- * ISSSO NAO FUNCIONOU POIS QUANDO UTILIZAMOS AS CHAVES, QUE TAMBEM FAZ
- * ELE ACABA FAZENDO PARTE DO PROPRIO CORPO DA FUNCTION, ENTAO NAO PODEMOS
- * UTILIZAR CHAVES PARA O OBJETO 
- * 
- * PARA ISSO TEMOS OUTRAS OPÇOES:
+ * resultado: NaN
+ * esse resultado significa que é um resultado descnhecido,
+ * por exemplo que ele nao conseguiu realizar a soma, entao
+ * por consequencia ele retorna essa expressao 'NaN'
 */
-//opcao 1
-const retObj = () =>{
-  return {nome:'gabriel'};
-} 
-console.log(retObj());
 
-//opcao 2
-const retObj2 = () =>({nome:'gabriel2 '});
+//Para isso não acontecer denifimos os valores padroes;
+function somaSemError(a = 2, b = 3){
+  return a + b;
+}
+console.log(somaSemError(1));
+console.log(somaSemError());
 
-console.log(retObj2());
+
+//UTILIZANDO A ARROW FUNCTIONS
+ const somaArrowFunc = (a = 0, b = 1) => {
+   return a + b;
+ }
+ console.log(somaArrowFunc(1));
+ console.log(somaArrowFunc());
