2c309b2da204bec99a6c5262a2148986f9b9aa18
gabrielbriks <greis9010@gmail.com>
2019-12-29 12:39:13 -0300

    SPRED - entendendo como e quais formas de utilizar o REST

--- bundle.js
+++ bundle.js
@@ -1,3 +1,9 @@
+"use strict";
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 /**
  * REST / SPREAD
  * como ainda não foram acoplados a função do Babel, precisaremos
@@ -5,4 +11,45 @@
  * yarn add @babel/plugin-proposal-object-rest-spread
  *
 */
-"use strict";
+//REST
+
+/**
+ * o REST serve para pegar o resto das propiedades
+ * EXEMPLO:
+ */
+var usuario = {
+  nome: 'Gabriel',
+  idade: 22,
+  Empresa: 'Polariuns'
+}; //utilizando o REST
+
+var nome = usuario.nome,
+    resto = _objectWithoutProperties(usuario, ["nome"]); //  console.log(nome);
+//  console.log(resto);
+//podemos aplicar isso em vetores
+
+
+var arr = [1, 2, 3, 4];
+var a = arr[0],
+    b = arr[1],
+    cRest = arr.slice(2); // console.log(a);
+// console.log(b);
+// console.log(cRest);
+//utilizando para parametro de functions
+
+function soma(a, b, c) {
+  return a + b + c;
+} // console.log(soma(1, 2, 3));
+//utilizando REST
+
+
+function somaRest() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  //utilizando o reduce para realizar a soma
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+} // console.log( somaRest(1, 2, 3) );
--- main.js
+++ main.js
@@ -5,3 +5,47 @@
  * yarn add @babel/plugin-proposal-object-rest-spread
  *
 */ 
+
+//REST
+/**
+ * o REST serve para pegar o resto das propiedades
+ * EXEMPLO:
+ */
+
+ const usuario = {
+  nome: 'Gabriel',
+  idade: 22,
+  Empresa: 'Polariuns'
+ };
+//utilizando o REST
+ const {nome, ...resto} = usuario;
+//  console.log(nome);
+//  console.log(resto);
+
+//podemos aplicar isso em vetores
+const arr = [1,2,3,4];
+const [a,b,...cRest] = arr;
+// console.log(a);
+// console.log(b);
+// console.log(cRest);
+
+
+//utilizando para parametro de functions
+function soma(a, b, c ){
+  return a + b + c;
+}
+// console.log(soma(1, 2, 3));
+
+//utilizando REST
+function somaRest(...params){
+  //utilizando o reduce para realizar a soma
+  return params.reduce((total,next) => total + next);
+}
+// console.log( somaRest(1, 2, 3) );
+
+
+
+
+
+
+
