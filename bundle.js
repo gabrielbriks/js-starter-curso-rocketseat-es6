e6d2040aa212f9a0e6c71563824f0b389dd1c638
gabrielbriks <greis9010@gmail.com>
2019-12-29 12:24:19 -0300

    operadores Rest/spread -  initial; instalando pacote  " @babel/plugin-proposal-object-rest-spread "

--- bundle.js
+++ bundle.js
@@ -1,36 +1,8 @@
-"use strict";
-
-//DESESTRUTURAÇÃO DE OBJETOS
-var usuario = {
-  nome: 'Gabriel',
-  idade: 22,
-  endereco: {
-    cidade: 'Brasilia',
-    estado: 'DF'
-  }
-};
 /**
- como poderiamos buscar certas informações desse usuario;
- a primeira ideia que se vem na cabeça é :
-   
-  const nome = usuario.nome;
-  const idade = usuario.idade;
-  const cidade = usuario.endereço.cidade;
- */
-// utilizando a destruturação de objeto
-
-var nome = usuario.nome,
-    idade = usuario.idade,
-    cidade = usuario.endereco.cidade; // console.log(nome);
-// console.log(idade);
-// console.log(cidade);
-//podemos usar essa abordagem em diversos locais ..
-
-function mostraNome(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade,
-      cidade = _ref.endereco.cidade;
-  console.log(nome, idade, cidade);
-}
-
-mostraNome(usuario);
+ * REST / SPREAD
+ * como ainda não foram acoplados a função do Babel, precisaremos
+ * utilizar um novo pacote 
+ * yarn add @babel/plugin-proposal-object-rest-spread
+ *
+*/
+"use strict";
--- main.js
+++ main.js
@@ -1,32 +1,7 @@
-//DESESTRUTURAÇÃO DE OBJETOS
-
-const usuario = {
-  nome: 'Gabriel',
-  idade: 22,
-  endereco: {
-    cidade: 'Brasilia',
-    estado: 'DF',
-  },
-};
 /**
- como poderiamos buscar certas informações desse usuario;
- a primeira ideia que se vem na cabeça é :
-   
-  const nome = usuario.nome;
-  const idade = usuario.idade;
-  const cidade = usuario.endereço.cidade;
- */
-
-// utilizando a destruturação de objeto
-const { nome, idade, endereco:{ cidade } } = usuario;
-// console.log(nome);
-// console.log(idade);
-// console.log(cidade);
-
-//podemos usar essa abordagem em diversos locais ..
-
-function mostraNome({nome,idade, endereco:{ cidade } }){
-  console.log(nome, idade,cidade);
-}
-mostraNome(usuario);
- 
+ * REST / SPREAD
+ * como ainda não foram acoplados a função do Babel, precisaremos
+ * utilizar um novo pacote 
+ * yarn add @babel/plugin-proposal-object-rest-spread
+ *
+*/ 
