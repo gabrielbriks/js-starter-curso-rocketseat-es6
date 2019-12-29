243dd18bf95cf0b729b10fb0d7f0f88f148a58e9
gabrielbriks <greis9010@gmail.com>
2019-12-29 10:16:41 -0300

    Arrow Functions - entendendo Arrow Functions

--- bundle.js
+++ bundle.js
@@ -1,50 +1,75 @@
 "use strict";
 
-//antes do ES6 utilizava-mos para varrer, alterar ou até mesmo pegar conteudos de 
-//arrays e etc, algumas funções como o foreach e etc.
-//No ES6 podemos utilizar algumas abordagens bem mais simples
-var arr = [1, 2, 3, 4]; //Utilizando a a função 'map()'
-//A função map recebe uma function qual ira receber os itens em questao,
-//tornando possivel o manuseio dos dados do array
+var arr = [1, 2, 3, 4, 5]; // ---- ARROW FUNCTIONS----
 
-var newArr = arr.map(function (item, index) {
-  //neste caso ele esta pegando cada item e mutiplicando por 2
-  return item + index;
+/**quando temos uma função anonima, eé o melhor local para utilizarmos
+* nossas arrow functions; da seguinte formas:
+*/
+//retirando o nome function
+
+var newArr = arr.map(function (item) {
+  return item * 2;
 });
-console.log(newArr);
+console.log(newArr); //por termo apenas um parametro, podemos retirar os parenteses
+
+var newArr2 = arr.map(function (item) {
+  return item * 3;
+});
+console.log(newArr2);
 /**
- *  --REDUCE-- 
- * Nessa funcao ele irá varrer o array realizando a soma do valor de cada rodada
- * (ou seja somar toddos os valores dentro do array)
- * determinando o resultado para o total, para isso estará sempre pegando
- * o proximo numero  
- */
+ * quando nossa function retorna uma simples informação,
+ * aí entao podemos reduzir ainda mais a nossa function; 
+*/
 
-var sum = arr.reduce(function (total, next) {
-  return total + next;
+var newArr3 = arr.map(function (item) {
+  return item * 4;
 });
-console.log(sum); //t:0 ; n:1
-//t:1 ; n:2
-//t:3 ; n:3
-//t:6 ; n:4
-//t10 ; n:0
+console.log(newArr3); //é possivel tambem utilizar-mos a 'const' para declarar functions
+//da seguinte maneira: ; apesar de nao ser recomendado...
+
+var teste = function teste() {
+  return 'teste';
+}; //EM VEZ DE
+// @ Assim fica mais bonito 
+// function teste(){
+// }
+
+
+console.log(teste()); //CASO QUISESSIMOS RETURNAR UM ARRAY, COM ESSA NOMENCLAURA:
+
+var teste2 = function teste2() {
+  return [1, 2, 5];
+};
+
+console.log(teste2()); //E SE TENTARMOS RETORNAR UM OBJETO DA SEGUINTE MANEIRA?
+
+var testeOBJ = function testeOBJ() {
+  nome: 'gabriel';
+};
+
+console.log(testeOBJ()); //resultado : undefined
 
 /**
- * Utilizando a funcao filter, nesse caso estamos utilizando ela para retornoar todos
- * is numeros pares do nosso array
- */
+ * ISSSO NAO FUNCIONOU POIS QUANDO UTILIZAMOS AS CHAVES, QUE TAMBEM FAZ
+ * ELE ACABA FAZENDO PARTE DO PROPRIO CORPO DA FUNCTION, ENTAO NAO PODEMOS
+ * UTILIZAR CHAVES PARA O OBJETO 
+ * 
+ * PARA ISSO TEMOS OUTRAS OPÇOES:
+*/
+//opcao 1
 
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter);
-/**
- * A função 'find()' nos ajuda a verificar se possui uma certa informacao dentro do array
- * ou se a gente consegue encontrar essa informação
- */
+var retObj = function retObj() {
+  return {
+    nome: 'gabriel'
+  };
+};
 
-var find = arr.find(function (item) {
-  //Nesse caso irá retornar todos o itens que forem menor ou igual a 4
-  return item <= 4;
-});
-console.log(find);
+console.log(retObj()); //opcao 2
+
+var retObj2 = function retObj2() {
+  return {
+    nome: 'gabriel2 '
+  };
+};
+
+console.log(retObj2());
--- main.js
+++ main.js
@@ -1,53 +1,68 @@
-//antes do ES6 utilizava-mos para varrer, alterar ou até mesmo pegar conteudos de 
-//arrays e etc, algumas funções como o foreach e etc.
+const arr = [1,2,3,4,5];
+// ---- ARROW FUNCTIONS----
+/**quando temos uma função anonima, eé o melhor local para utilizarmos
+* nossas arrow functions; da seguinte formas:
+*/
 
-//No ES6 podemos utilizar algumas abordagens bem mais simples
-const arr = [1,2,3,4];
-//Utilizando a a função 'map()'
-//A função map recebe uma function qual ira receber os itens em questao,
-//tornando possivel o manuseio dos dados do array
-const newArr = arr.map(function(item, index){
-  //neste caso ele esta pegando cada item e mutiplicando por 2
-  return item + index;
+//retirando o nome function
+const newArr = arr.map((item)=>{
+  return item * 2;
 });
 
 console.log(newArr);
 
-/**
- *  --REDUCE-- 
- * Nessa funcao ele irá varrer o array realizando a soma do valor de cada rodada
- * (ou seja somar toddos os valores dentro do array)
- * determinando o resultado para o total, para isso estará sempre pegando
- * o proximo numero  
- */
-const sum  = arr.reduce(function(total, next){
-  return total + next;
-});
-console.log(sum);
-
-//t:0 ; n:1
-//t:1 ; n:2
-//t:3 ; n:3
-//t:6 ; n:4
-//t10 ; n:0
-
-/**
- * Utilizando a funcao filter, nesse caso estamos utilizando ela para retornoar todos
- * is numeros pares do nosso array
- */
-const filter = arr.filter(function(item){
-  return item % 2 === 0;
+//por termo apenas um parametro, podemos retirar os parenteses
+const newArr2 = arr.map(item=>{
+  return item * 3;
 });
 
-console.log(filter);
+console.log(newArr2);
 
 /**
- * A função 'find()' nos ajuda a verificar se possui uma certa informacao dentro do array
- * ou se a gente consegue encontrar essa informação
- */
-const find = arr.find(function(item){
-  //Nesse caso irá retornar todos o itens que forem menor ou igual a 4
-  return item <= 4;
-});
+ * quando nossa function retorna uma simples informação,
+ * aí entao podemos reduzir ainda mais a nossa function; 
+*/
+const newArr3 = arr.map(item => item * 4);
 
-console.log(find);
+console.log(newArr3);
+
+//é possivel tambem utilizar-mos a 'const' para declarar functions
+//da seguinte maneira: ; apesar de nao ser recomendado...
+const teste = () => {
+  return 'teste';
+}
+//EM VEZ DE
+// @ Assim fica mais bonito 
+// function teste(){
+
+// }
+console.log(teste());
+
+//CASO QUISESSIMOS RETURNAR UM ARRAY, COM ESSA NOMENCLAURA:
+
+const teste2 = () => [1,2,5];
+console.log(teste2())
+
+//E SE TENTARMOS RETORNAR UM OBJETO DA SEGUINTE MANEIRA?
+const testeOBJ = () => {nome:'gabriel'};
+
+console.log(testeOBJ());
+//resultado : undefined
+
+/**
+ * ISSSO NAO FUNCIONOU POIS QUANDO UTILIZAMOS AS CHAVES, QUE TAMBEM FAZ
+ * ELE ACABA FAZENDO PARTE DO PROPRIO CORPO DA FUNCTION, ENTAO NAO PODEMOS
+ * UTILIZAR CHAVES PARA O OBJETO 
+ * 
+ * PARA ISSO TEMOS OUTRAS OPÇOES:
+*/
+//opcao 1
+const retObj = () =>{
+  return {nome:'gabriel'};
+} 
+console.log(retObj());
+
+//opcao 2
+const retObj2 = () =>({nome:'gabriel2 '});
+
+console.log(retObj2());
