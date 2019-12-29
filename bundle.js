c1edac09a3e4da3d6fc9309e592ad52ae7805358
gabrielbriks <greis9010@gmail.com>
2019-12-29 09:49:00 -0300

    operações com array, -  map, reduce, filter e find

--- bundle.js
+++ bundle.js
@@ -1,31 +1,50 @@
 "use strict";
 
-//constantes e variaveis de escopos
+//antes do ES6 utilizava-mos para varrer, alterar ou até mesmo pegar conteudos de 
+//arrays e etc, algumas funções como o foreach e etc.
+//No ES6 podemos utilizar algumas abordagens bem mais simples
+var arr = [1, 2, 3, 4]; //Utilizando a a função 'map()'
+//A função map recebe uma function qual ira receber os itens em questao,
+//tornando possivel o manuseio dos dados do array
 
-/*const
-  Nao pode ter o seu valor reatribuido;
-  const a = 3 ;
+var newArr = arr.map(function (item, index) {
+  //neste caso ele esta pegando cada item e mutiplicando por 2
+  return item + index;
+});
+console.log(newArr);
+/**
+ *  --REDUCE-- 
+ * Nessa funcao ele irá varrer o array realizando a soma do valor de cada rodada
+ * (ou seja somar toddos os valores dentro do array)
+ * determinando o resultado para o total, para isso estará sempre pegando
+ * o proximo numero  
+ */
 
-  ERROR
-  a = 5;
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum); //t:0 ; n:1
+//t:1 ; n:2
+//t:3 ; n:3
+//t:6 ; n:4
+//t10 ; n:0
 
-  UNICA FORMA DE ALTERA-LÁ E MUTANDO ELA CONFORME A BORDAGEM ABAIXO;
-*/
-var usuario = {
-  nome: 'Gabriel'
-};
-usuario.nome = 'Cleiton';
-console.log(usuario.nome); //VARIAVEIS DE ESCOPO
+/**
+ * Utilizando a funcao filter, nesse caso estamos utilizando ela para retornoar todos
+ * is numeros pares do nosso array
+ */
 
-function teste(x) {
-  //declarando minha variavel de escopo
-  var y = 2;
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+/**
+ * A função 'find()' nos ajuda a verificar se possui uma certa informacao dentro do array
+ * ou se a gente consegue encontrar essa informação
+ */
 
-  if (x > 5) {
-    //atribuindo um novo valor para a minha variavel 
-    var _y = 4;
-    console.log(x, _y);
-  }
-}
-
-teste(10);
+var find = arr.find(function (item) {
+  //Nesse caso irá retornar todos o itens que forem menor ou igual a 4
+  return item <= 4;
+});
+console.log(find);
--- main.js
+++ main.js
@@ -1,34 +1,53 @@
-//constantes e variaveis de escopos
+//antes do ES6 utilizava-mos para varrer, alterar ou até mesmo pegar conteudos de 
+//arrays e etc, algumas funções como o foreach e etc.
 
-/*const
-  Nao pode ter o seu valor reatribuido;
-  const a = 3 ;
+//No ES6 podemos utilizar algumas abordagens bem mais simples
+const arr = [1,2,3,4];
+//Utilizando a a função 'map()'
+//A função map recebe uma function qual ira receber os itens em questao,
+//tornando possivel o manuseio dos dados do array
+const newArr = arr.map(function(item, index){
+  //neste caso ele esta pegando cada item e mutiplicando por 2
+  return item + index;
+});
 
-  ERROR
-  a = 5;
+console.log(newArr);
 
-  UNICA FORMA DE ALTERA-LÁ E MUTANDO ELA CONFORME A BORDAGEM ABAIXO;
-*/
+/**
+ *  --REDUCE-- 
+ * Nessa funcao ele irá varrer o array realizando a soma do valor de cada rodada
+ * (ou seja somar toddos os valores dentro do array)
+ * determinando o resultado para o total, para isso estará sempre pegando
+ * o proximo numero  
+ */
+const sum  = arr.reduce(function(total, next){
+  return total + next;
+});
+console.log(sum);
 
+//t:0 ; n:1
+//t:1 ; n:2
+//t:3 ; n:3
+//t:6 ; n:4
+//t10 ; n:0
 
-const usuario = {nome: 'Gabriel'};
+/**
+ * Utilizando a funcao filter, nesse caso estamos utilizando ela para retornoar todos
+ * is numeros pares do nosso array
+ */
+const filter = arr.filter(function(item){
+  return item % 2 === 0;
+});
 
-usuario.nome = 'Cleiton';
+console.log(filter);
 
-console.log(usuario.nome);
+/**
+ * A função 'find()' nos ajuda a verificar se possui uma certa informacao dentro do array
+ * ou se a gente consegue encontrar essa informação
+ */
+const find = arr.find(function(item){
+  //Nesse caso irá retornar todos o itens que forem menor ou igual a 4
+  return item <= 4;
+});
 
-
-//VARIAVEIS DE ESCOPO
-function teste(x){
-  //declarando minha variavel de escopo
-  let y = 2;
-
-  if(x > 5){
-    //atribuindo um novo valor para a minha variavel 
-    let y = 4;
-
-    console.log(x, y);
-  }
-  
-}
-teste(10);
+console.log(find);
