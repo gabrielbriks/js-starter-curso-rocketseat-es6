3ce471c4c998c38fda628aa83df2ff8f808eff3d
gabrielbriks <greis9010@gmail.com>
2020-01-04 16:50:09 -0300

    Async/await - aplicando Async/await  com axios e a API GitHub

--- src/main.js
+++ src/main.js
@@ -1,47 +1,20 @@
-const minhaPromise = () => new Promise((resolve,reject) =>{
-  setTimeout(() => { resolve('OK') }, 2000);
-});
+//Importando o axios
+import axios from 'axios';
 
-//utilizando a sintax normal normal
-minhaPromise().then(response => {
-  console.log(response);
+//Criando uma classe
+class Api {
+  //Para trtar o erro nesse caso a melhor opção é utilizar o try catch
   
-  minhaPromise().then(response => {
-    console.log(response);
-
-    minhaPromise().then(response => {
+  //Criando um metodo static que recebera o usuario 
+  static async getUserInfo(username){
+    try {
+      const response =  await axios.get(`https://api.github.com/users/${username}`);
       console.log(response);
-    });
-  });
-});
-
-/**
- * Ao inves de usarmos o .then e .cacth varias vezes conforme o exemplo acima, utilizamos apenas 
- * o await conforme o exemplo abaixo;
- * 
- * Automaticamente quando utilizando o async para uma function, ela vira uma promise, sendo assim,
- * poderemos esperar o resultado dessa function utilizando o await 
- * 
- * OBS: So se pode usar o await na frende de uma promise se caso estivermos dentro de uma async,
- * 
- */
-
-
-// async function executaPromise(){
-//   console.log( await minhaPromise());
-//   console.log( await minhaPromise());
-//   console.log( await minhaPromise());
-  
-// }
-// executaPromise();
-// Exemplo da espera da resposta com await na frente de uma promise, que nao está dentro de um
-// async .. resultará em error
-//               await executaPromise();
-
-//podemos converter o exemplo acima para um arrow functions
-const executaPromise = async () =>{
-  console.log( await minhaPromise());
-  console.log( await minhaPromise());
-  console.log( await minhaPromise());
+    }catch(err){
+      console.warn('ERROR na API ');
+    }
+  }
 }
-executaPromise();
+//Chamando o nosso metodo da da classe Api
+Api.getUserInfo('gabrielbriks');
+// Api.getUserInfo('');
